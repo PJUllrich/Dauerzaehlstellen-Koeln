5b4e9d5b53933753d8ae74717d68496907d0d8bb
Peter Ullrich <peter.j.ullrich@gmail.com>
2020-04-11 14:09:16 +0200

    Added data

--- scripts/hole_daten.py
+++ scripts/hole_daten.py
@@ -10,26 +10,29 @@ DATEN_FOLDER_PATH = 'Daten/'
 
 
 # Hole Daten zu allen Dauerzaehlstellen
-def hole_zaehler_uebersicht():
+def hole_zaehler_uebersicht(save=True):
     r = requests.post(URL_COUNTER_LIST, data={'id': ID_KOELN})
-    with open(DATEN_FOLDER_PATH + 'counter_list.json', 'w') as f:
-        json.dump(r.json(), f)
+    if save:
+        with open(DATEN_FOLDER_PATH + 'counter_list.json', 'w') as f:
+            json.dump(r.json(), f)
+    else:
+        return r.json()
 
 
 # Holt die Zählerstände pro Tag für den Vorgebirgspark
-def hole_zaehler_details():
+def hole_zaehler_details(idPdc, von, bis, filename, interval=4):
     # interval: 4 = taeglich, 5 = woechentlich, 6 = monatlich
     # idPdc: 100019755 scheint die Id fuer den Zaehler im Vorgebirgspark zu sein
     r = requests.post(URL_COUNTER_DATA, data={
         'idOrganisme': ID_KOELN,
-        'idPdc': '100019755',
-        'fin': '11/04/2020',
-        'debut': '01/01/2020',
-        'interval': 4,
+        'idPdc': idPdc,
+        'debut': von,
+        'fin': bis,
+        'interval': interval,
         'pratiques': ''
     })
 
-    with open(DATEN_FOLDER_PATH + 'vorgebirgspark.csv', mode='w') as f:
+    with open(DATEN_FOLDER_PATH + filename + '.csv', mode='w') as f:
         writer = csv.writer(
             f, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
@@ -40,5 +43,15 @@ def hole_zaehler_details():
                 writer.writerow(row)
 
 
+def hole_alle_zaehler_details():
+    von = '01/01/2020'
+    bis = '11/04/2020'
+    uebersicht = hole_zaehler_uebersicht(save=False)
+    for row in uebersicht:
+        idPdc = row['idPdc']
+        filename = row['nom'].replace(' ', '_').lower()
+        hole_zaehler_details(idPdc, von, bis, filename)
+
+
 if __name__ == "__main__":
-    hole_zaehler_details()
+    hole_alle_zaehler_details()
