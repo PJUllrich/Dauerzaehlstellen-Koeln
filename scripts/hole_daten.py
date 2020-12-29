b1acf31a01d2075dd8d9565ae122dd4e71f068be
Peter Ullrich <peter.j.ullrich@gmail.com>
2020-12-29 17:37:59 +0100

    Fix script

--- scripts/hole_daten.py
+++ scripts/hole_daten.py
@@ -22,7 +22,7 @@ def get(url, params=None):
 def hole_datum_des_letzten_updates():
     try:
         with open(DATEN_FOLDER_PATH + 'letztes_update.txt', "r") as f:
-            return datetime.strptime(f.read(), "%d/%m/%Y")
+            return f.read()
     except IOError:
         return None
 
@@ -36,9 +36,9 @@ def hole_daten(von, bis):
     uebersicht = hole_zaehler_uebersicht(save=False)
     for row in uebersicht:
         idPdc = row['idPdc']
-        cumulFlowId = row['cumulFlowId']
+        flowIds = ";".join([str(flowId['id']) for flowId in row['pratique']])
         filename = row['nom'].replace(' ', '_').lower()
-        hole_zaehler_details(cumulFlowId, idPdc, von, bis, filename)
+        hole_zaehler_details(idPdc, flowIds, von, bis, filename)
 
 
 # Hole Daten zu allen Dauerzaehlstellen
@@ -52,7 +52,7 @@ def hole_zaehler_uebersicht(save=False):
 
 
 # Holt die Zählerstände fuer einen Zaehler und speichert diese
-def hole_zaehler_details(cumulFlowId, idPdc, von, bis, filename, append=True, interval=4):
+def hole_zaehler_details(idPdc, flowIds, von, bis, filename, append=True, interval=4):
     # interval: 4 = taeglich, 5 = woechentlich, 6 = monatlich
     params = {
         'idOrganisme': ID_KOELN,
@@ -60,9 +60,9 @@ def hole_zaehler_details(cumulFlowId, idPdc, von, bis, filename, append=True, in
         'debut': von,
         'fin': bis,
         'interval': interval,
-        'pratiques': idPdc
+        'flowIds': flowIds
     }
-    url = f'{URL_COUNTER_DATA}/{cumulFlowId}'
+    url = f'{URL_COUNTER_DATA}/{idPdc}'
     r = get(url, params)
 
     mode = 'a' if append else 'w'
@@ -74,13 +74,16 @@ def hole_zaehler_details(cumulFlowId, idPdc, von, bis, filename, append=True, in
         if not append:
             writer.writerow(['Datum', 'Zaehlerstand'])
 
-        for row in r.json():
-            if isinstance(row, Iterable):
-                date = datetime.strptime(row[0], "%m/%d/%Y")
-                date_german = date.strftime("%d.%m.%Y")
-                count = int(float(row[1]))
+        try:
+            for row in r.json():
+                if isinstance(row, Iterable):
+                    date = datetime.strptime(row[0], "%m/%d/%Y")
+                    date_german = date.strftime("%d.%m.%Y")
+                    count = int(float(row[1]))
 
-                writer.writerow([date_german, count])
+                    writer.writerow([date_german, count])
+        except json.decoder.JSONDecodeError:
+            print(f'{idPdc}: {r.text}')
 
 
 # Holt alle Zaehlerstaende fuer einen Zeitraum fuer alle Zaehler
@@ -91,21 +94,15 @@ def hole_alle_zaehler_details():
     hole_daten(von, bis)
 
 
-def hole_daten_fuer_gestern():
-    gestern = datetime.today() - timedelta(days=1)
-    letztes_update_am = hole_datum_des_letzten_updates()
+def hole_neue_daten():
+    heute = datetime.today()
+    str_datum_heute = heute.strftime("%d/%m/%Y")
+    str_datum_letztes = hole_datum_des_letzten_updates()
 
-    str_datum_gestern = gestern.strftime("%d/%m/%Y")
+    hole_daten(str_datum_letztes, str_datum_heute)
 
-    if letztes_update_am is None or letztes_update_am.date() == gestern.date():
-        hole_daten(str_datum_gestern, str_datum_gestern)
-    elif letztes_update_am.date() < gestern.date():
-        datum_ab_wann_daten_fehlen = letztes_update_am + timedelta(days=1)
-        str_datum_fehlend = datum_ab_wann_daten_fehlen.strftime("%d/%m/%Y")
-        hole_daten(str_datum_fehlend, str_datum_gestern)
-
-    update_datum_des_letzten_updates(gestern)
+    update_datum_des_letzten_updates(heute)
 
 
 if __name__ == "__main__":
-    hole_daten_fuer_gestern()
+    hole_neue_daten()
